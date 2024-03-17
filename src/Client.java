fbc73c7bae1683dda877ba46f0caac1a2178493d
ferfolya23 <ferfolya23@gmail.com>
2024-03-16 23:13:50 -0400

    LibraryHomePage corrections
    - moved LibraryHomePage initialization into Client
    - corrected a couple of bugs that led to csv file being overwritten
    - made it so when registering, LibraryHomePage checks if account linked to the email typed in is already in the database

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,8 @@
-
 public class Client {
-
+	public static void main(String [] args) throws Exception{
+		String path = "src/account_data.csv";
+		LibraryHomePage homePage = new LibraryHomePage();
+		
+		homePage.load(homePage, path);
+	}
 }
--- src/LibraryHomePage.java
+++ src/LibraryHomePage.java
@@ -8,23 +8,141 @@ import java.util.Scanner;
 
 public class LibraryHomePage {
 	
-	static LibraryHomePage homePage;
-	public static ArrayList<Account> users = new ArrayList<Account>();
-	public static String path;
-    protected boolean isValid;
-    static Scanner input = new Scanner(System.in);
+	LibraryHomePage homePage;
+	public ArrayList<Account> users;
+	public String path;
+    Scanner input;
 	
-	public static void main(String [] args) throws Exception{
-		String path = "src/account_data.csv";
-		homePage = new LibraryHomePage();
-		homePage.load(path);
+	public void load(LibraryHomePage homePage, String path) throws Exception{
+		users = new ArrayList<Account>();
+		this.homePage = homePage;
+		this.path = path;
+		input = new Scanner(System.in);
+		CsvReader reader = new CsvReader(path);
+		reader.readHeaders();
+		
+		while(reader.readRecord()){
+			
+			String email = reader.get("email");
+			String password = reader.get("password");
+			String accType = reader.get("accType");
+			accountGenerator(email, password, accType);
+		}
+		
 		loggedOutHomePage();
 	}
 	
-	
-	public static Account accountGenerator(String email, String password, String accType) throws Exception {
+	public void update() throws Exception{
+		try {		
+				CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
+				//email,password,accType
+				csvOutput.write("email");
+				csvOutput.write("password");
+		    	csvOutput.write("accType");
+				csvOutput.endRecord();
+
+				// else assume that the file already has the correct header line
+				// write out a few records
+				for(Account u: users){
+					csvOutput.write(u.getEmail());
+					csvOutput.write(u.getPass());
+					csvOutput.write(u.getAccType());
+					csvOutput.endRecord();
+				}
+				csvOutput.close();
+			
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
+
+    protected void loggedOutHomePage() throws Exception {
+        
+    	while (true) {
+        	System.out.println("L = Log In");
+            System.out.println("R = Register");
+            String nextPage = input.nextLine();
+            
+            if (nextPage.equals("L")) {
+            	login();
+            }
+            
+            if (nextPage.equals("R")) {
+            	register();
+            }
+            else {
+            	System.out.println("Please enter a valid input!");
+            }
+            
+    	}
+    }
+    
+    protected void loggedInHomePage() {
+    	System.out.println("Login completed!");
+    	System.exit(0);
+    	
+    	// TODO
+    	// Displays list of hard cover books that a user is renting, plus their due dates.
+    	// Prompts warnings for books with due dates less than 24 hours.
+    	// Prompts DIFF warning if book is past due date.
+    	// Also contains buttons for accessing common user tasks like search, rent, etc.
+    }
+    
+    protected void register() throws Exception{
+    	while (true) {
+        	System.out.println("Enter email: ");
+            String email = input.nextLine();
+            
+            System.out.println("Enter password: ");
+            String password = input.nextLine();
+            
+            // This would be replaced with a button in implementation.
+            System.out.println("Select Account Type: ");
+            String accType = input.nextLine();
+            
+            Account accountExists = iterateDB(email, password);
+            
+            if (accountExists != null) {
+            	System.out.println("You already have an account. Please try logging in instead!");
+            	loggedOutHomePage();
+            }
+            else {
+                // TODO - Add some validation method prior to account creation if not Visitor.
+        		Account account = accountGenerator(email, password, accType);
+        		homePage.update();
+        		System.out.println("Registration successful!");
+                loggedOutHomePage();
+            }
+    	}
+    }
+    
+    protected void login() throws Exception{
+    	Account infoExists = null;
+    	
+    	while (true) {
+    		
+    		System.out.println("Enter email:");
+            String email = input.nextLine();
+            
+            System.out.println("Enter password:");
+            String password = input.nextLine();
+            
+            infoExists = iterateDB(email, password);
+            
+            if (infoExists != null) {
+            	break;
+            }
+            
+            System.out.println("Incorrect credentials, please try again!");
+    	}
+    	
+    	// Maybe have infoExists provided as input to load user profile to loggedInHomePage.
+    	loggedInHomePage();
+    }
+    
+	public Account accountGenerator(String email, String password, String accType) {
 		
-		Account user;
+		Account user = null;
 		
 	    if (accType.equals("Visitor")) {
 	    	user = new Visitor(email, password, accType);
@@ -45,129 +163,36 @@ public class LibraryHomePage {
 	    	users.add(user);
 	    }
 	    
-	    else {
-	    	throw new Exception("Invalid Account Type");
-	    }
+//	    else {
+//	    	throw new Exception("Invalid Account Type");
+//	    }
 	    
 		return user;
 	}
 	
-	public boolean iterateDB(String path, String email, String password) throws Exception{
-		CsvReader reader = new CsvReader(path); 
-		reader.readHeaders();
+	public Account iterateDB(String email, String password) throws Exception{
 		
-		while(reader.readRecord()){
-			String DB_email = reader.get("email");
-			String DB_password = reader.get("password");
-			
-			if (DB_email.equals(email) && DB_password.equals(password)) {
-				return true;
-			}
+		int i = 0;
+		Account account;
+		while (i < users.size()) {
+		    account = users.get(i);
+		    
+		    String DB_email = account.getEmail();
+		    String DB_password = account.getPass();
+		    
+		    if (email.equals(DB_email)) {
+		    	if (password.equals(DB_password)) {
+		    		return account;
+		    	}
+		    	else {
+		    		System.out.println("You already have an account linked to this email, but your password is incorrect.");
+		    		loggedOutHomePage();
+		    	}
+		    }
+		    
+		    i++;
 		}
 		
-		return false;
+		return null;
 	}
-	
-	public void load(String path) throws Exception{
-		CsvReader reader = new CsvReader(path); 
-		reader.readHeaders();
-		
-		while(reader.readRecord()){
-			
-			String email = reader.get("email");
-			String password = reader.get("password");
-			String accType = reader.get("accType");
-			accountGenerator(email, password, accType);
-		}
-	}
-	
-	public void update(String path) throws Exception{
-		try {		
-				CsvWriter csvOutput = new CsvWriter(new FileWriter(path, false), ',');
-				//name,id,email,password
-				csvOutput.write("name");
-				csvOutput.write("id");
-		    	csvOutput.write("email");
-				csvOutput.write("password");
-				csvOutput.endRecord();
-
-				// else assume that the file already has the correct header line
-				// write out a few records
-				for(Account u: users){
-					csvOutput.write(u.getEmail());
-					csvOutput.write(u.getPass());
-					csvOutput.write(u.getAccType());
-					csvOutput.endRecord();
-				}
-				csvOutput.close();
-			
-			}catch (Exception e) {
-				e.printStackTrace();
-			}
-	}
-
-    protected static void loggedOutHomePage() throws Exception {
-        System.out.println("L = Log In");
-        System.out.println("R = Register");
-        String nextPage = input.nextLine();
-        
-        if (nextPage.equals("L")) {
-        	login();
-        }
-        
-        if (nextPage.equals("R")) {
-        	register();
-        }
-    }
-    
-    protected static void loggedInHomePage() {
-    	System.out.println("Login completed!");
-    	System.exit(0);
-    	
-    	// TODO
-    	// Displays list of hard cover books that a user is renting, plus their due dates.
-    	// Prompts warnings for books with due dates less than 24 hours.
-    	// Prompts DIFF warning if book is past due date.
-    	// Also contains buttons for accessing common user tasks like search, rent, etc.
-    }
-    
-    protected static void register() throws Exception{
-        System.out.println("Enter email: ");
-        String email = input.nextLine();
-        
-        System.out.println("Enter password: ");
-        String password = input.nextLine();
-        
-        // This would be replaced with a button in implementation.
-        System.out.println("Select Account Type: ");
-        String accType = input.nextLine();
-        
-        // TODO - Add some validation method prior to account creation if not Visitor.
-		accountGenerator(email, password, accType);
-		homePage.update(path);
-        loggedOutHomePage();
-    }
-    
-    protected static void login() throws Exception{
-    	boolean isValid = false;
-    	
-    	while (true) {
-    		
-    		System.out.println("Enter email:");
-            String email = input.nextLine();
-            
-            System.out.println("Enter password:");
-            String password = input.nextLine();
-            
-            isValid = iterateDB(path, email, password);
-            
-            if (isValid) {
-            	break;
-            }
-            
-            System.out.println("Incorrect credentials, please try again!");
-    	}
-    	
-    	loggedInHomePage();
-    }
 }
