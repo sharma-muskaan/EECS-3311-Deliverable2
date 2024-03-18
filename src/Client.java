13c41d680e246937bee467566909fbce8831f687
ferfolya23 <ferfolya23@gmail.com>
2024-03-18 00:31:32 -0400

    Testing CSV file creation for new accounts
    added some code in Client to test CSV generation for digital and physical item lists since new CSVs will need to be made for every newly registered account

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,32 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Client {
 	public static void main(String [] args) throws Exception{
-		String path = "src/account_data.csv";
-		LibraryHomePage homePage = LibraryHomePage.getInstance(path);
+		//String path = "src/csv/account_data.csv";
+		
+		// Specify the file path
+		String filePath = "src/csv/";
+		String testFileName = "testFile" + ".csv";
+		filePath += testFileName;
+
+        // Create a File object
+        File file = new File(filePath);
+
+        try {
+            // Create the file
+            if (file.createNewFile()) {
+                System.out.println("File created successfully.");
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred while creating the file: " + e.getMessage());
+            e.printStackTrace();
+        }
+		
+		//LibraryHomePage homePage = LibraryHomePage.getInstance(path);
 	}
 }
+
+//boolean check = new File(directory, temp).exists();
