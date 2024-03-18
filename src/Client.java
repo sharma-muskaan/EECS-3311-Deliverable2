39fa486fb17a3b200f1b83c97154990b691af6ec
ferfolya23 <ferfolya23@gmail.com>
2024-03-18 03:28:57 -0400

    Removed no longer needed comment from Client

--- src/Client.java
+++ src/Client.java
@@ -1,29 +1,5 @@
-import java.io.File;
-import java.io.IOException;
-
 public class Client {
 	public static void main(String [] args) throws Exception{
-		
-//		// Specify the file path
-//		String filePath = "src/csv/";
-//		String testFileName = "testFile" + ".csv";
-//		filePath += testFileName;
-//
-//        // Create a File object
-//        File file = new File(filePath);
-//
-//        try {
-//            // Create the file
-//            if (file.createNewFile()) {
-//                System.out.println("File created successfully.");
-//            } else {
-//                System.out.println("File already exists.");
-//            }
-//        } catch (IOException e) {
-//            System.out.println("An error occurred while creating the file: " + e.getMessage());
-//            e.printStackTrace();
-//        }
-		
 		LibraryDatabase database = LibraryDatabase.getInstance();
 		LibraryHomePage homePage = LibraryHomePage.getInstance();
 		database.loadAccounts();
@@ -34,5 +10,3 @@ public class Client {
 		homePage.loggedOutHomePage();
 	}
 }
-
-//boolean check = new File(directory, temp).exists();
