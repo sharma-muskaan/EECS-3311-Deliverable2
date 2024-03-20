30d9f10703490c687da372c4bef1783f26460c20
ferfolya23 <ferfolya23@gmail.com>
2024-03-20 14:55:07 -0400

    Added courseEndDate to Course class
    - course methods are not finalized though

--- src/Course.java
+++ src/Course.java
@@ -1,26 +1,20 @@
 import java.util.ArrayList;
+import java.util.Date;
 
 public class Course {
 	protected String courseName;
 	protected DigitalItem currentCourseBook;
 	protected ArrayList<DigitalItem> courseBookHistory;
+	protected Date courseEndDate;
 	
-	public Course(String courseName) {
-		this.courseName = courseName;
-		courseBookHistory = new ArrayList<DigitalItem>();
-	}
-	
-	public Course(String courseName, ArrayList<DigitalItem> courseBooks) {
+	public Course(String courseName, ArrayList<DigitalItem> courseBooks, Date courseEndDate) {
 		this.courseName = courseName;
 		this.courseBookHistory = courseBooks;
+		this.courseEndDate = courseEndDate;
 	}
 
 	public String getCourseName() {
 		return courseName;
-	}
-
-	public void setCourseName(String courseName) {
-		this.courseName = courseName;
 	}
 
 	public ArrayList<DigitalItem> getCourseBooks() {
@@ -38,6 +32,28 @@ public class Course {
 	public void setCurrentCourseBook(DigitalItem currentCourseBook) {
 		this.currentCourseBook = currentCourseBook;
 	}
+	
+
+	public ArrayList<DigitalItem> getCourseBookHistory() {
+		return courseBookHistory;
+	}
+
+	public void setCourseBookHistory(ArrayList<DigitalItem> courseBookHistory) {
+		this.courseBookHistory = courseBookHistory;
+	}
+
+	public Date getCourseEndDate() {
+		return courseEndDate;
+	}
+
+	public void setCourseEndDate(Date courseEndDate) {
+		this.courseEndDate = courseEndDate;
+	}
+
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
 	
 	public void addCourseBook(DigitalItem courseBook) {
 		courseBookHistory.add(courseBook);
